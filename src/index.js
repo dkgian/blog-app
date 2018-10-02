c5909039d3ed811ce4d596eeee103ded78dc2f7d
dkgian <duykhanhgian0510@gmail.com>
2018-10-02 23:37:32 +0200

    add router /posts/new

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import promise from 'redux-promise'
 
 import './index.css'
@@ -10,7 +10,9 @@ import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
 import reducers from './reducers'
+
 import PostsIndex from './components/posts_index'
+import PostsNew from './components/posts_new'
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore)
 
@@ -18,7 +20,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex} />
+        <Switch>
+          <Route path="/" exact component={PostsIndex} />
+          <Route path="/posts/new" component={PostsNew} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>,
